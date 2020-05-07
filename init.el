43d09eac1c87dc362bd72f7770c0a9a3b190ded3
hirogwa <hirogwa@gmail.com>
2020-05-06 21:47:53 -0700

    Try using use-package

--- init.el
+++ init.el
@@ -1,11 +1,15 @@
 ;;; packages
 (setq custom-file "~/.emacs.d/custom.el")  ; I have no intention of using it for now
-
+(package-initialize)
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(package-initialize)
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-always-ensure t)
 
-(exec-path-from-shell-initialize)
+(use-package exec-path-from-shell
+  :config
+  (exec-path-from-shell-initialize))
 
 ;; open buffer in the same window
 (global-set-key "\C-x\C-b" 'buffer-menu)
@@ -26,57 +30,66 @@
 ;; theme. https://github.com/emacs-jp/replace-colorthemes/blob/master/dark-laptop-theme.el
 (load-file "~/.emacs.d/dark-laptop-theme.el")
 
-;;; window management
+;;; window navigation
 (split-window-right)                ; split vertically the window at startup
 (setq split-height-threshold 100)   ; and prevent commands from splitting further
-(require 'buffer-move)
-(global-set-key (kbd "<C-S-left>")   'buf-move-left)
-(global-set-key (kbd "<C-S-right>")  'buf-move-right)
-(global-set-key (kbd "<C-S-up>")  'buf-move-up)
-(global-set-key (kbd "<C-S-down>")  'buf-move-down)
-(setq buffer-move-behavior 'move)
-(windmove-default-keybindings)    ; move to other window by S-right etc.
+(windmove-default-keybindings)      ; move to other window by S-right etc.
 (winner-mode)
 (global-set-key (kbd "C-c p") 'winner-undo)
 (global-set-key (kbd "C-c n") 'winner-redo)
+(use-package buffer-move
+  :config
+  (global-set-key (kbd "<C-S-left>")   'buf-move-left)
+  (global-set-key (kbd "<C-S-right>")  'buf-move-right)
+  (global-set-key (kbd "<C-S-up>")  'buf-move-up)
+  (global-set-key (kbd "<C-S-down>")  'buf-move-down)
+  (setq buffer-move-behavior 'move))
 
 ;;; git
-(require 'magit)
-(global-set-key (kbd "C-x m") 'magit-status)
-(define-key magit-hunk-section-map (kbd "RET") 'magit-diff-visit-file-other-window)
+(use-package magit
+  :config
+  (global-set-key (kbd "C-x m") 'magit-status)
+  (define-key magit-hunk-section-map (kbd "RET") 'magit-diff-visit-file-other-window))
 
 ;;; org mode - override org-mode default to favor general window switching
-(require 'org)
-(define-key org-mode-map (kbd "<C-S-left>") nil)
-(define-key org-mode-map (kbd "<C-S-right>") nil)
-(define-key org-mode-map (kbd "<C-S-up>") nil)
-(define-key org-mode-map (kbd "<C-S-down>") nil)
-(define-key org-mode-map (kbd "<S-up>") nil)
-(define-key org-mode-map (kbd "<S-down>") nil)
-(define-key org-mode-map (kbd "<S-left>") nil)
-(define-key org-mode-map (kbd "<S-right>") nil)
+(use-package org
+  :config
+  (define-key org-mode-map (kbd "<C-S-left>") nil)
+  (define-key org-mode-map (kbd "<C-S-right>") nil)
+  (define-key org-mode-map (kbd "<C-S-up>") nil)
+  (define-key org-mode-map (kbd "<C-S-down>") nil)
+  (define-key org-mode-map (kbd "<S-up>") nil)
+  (define-key org-mode-map (kbd "<S-down>") nil)
+  (define-key org-mode-map (kbd "<S-left>") nil)
+  (define-key org-mode-map (kbd "<S-right>") nil))
 
 ;;; whitespaces
-(require 'whitespace)
-(setq whitespace-line-column -1)
-(setq whitespace-style `(face trailing tabs lines newline))
-(global-whitespace-mode 1)
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(use-package whitespace
+  :config
+  (setq whitespace-line-column -1)
+  (setq whitespace-style `(face trailing tabs lines newline))
+  (global-whitespace-mode 1))
 
 ;;; Python
-(require 'python)
-(define-key python-mode-map (kbd "C-x p") 'insert-pdb-set-trace)
 (defun insert-pdb-set-trace ()
   "Insert pdb."
   (interactive)
   (insert "import pdb; pdb.set_trace()"))
+(use-package python
+  :config
+  (define-key python-mode-map (kbd "C-x p") 'insert-pdb-set-trace))
 
 ;;; flycheck
-(global-flycheck-mode)
-(global-set-key (kbd "s-n") 'flycheck-next-error)
-(global-set-key (kbd "s-p") 'flycheck-previous-error)
-(global-set-key (kbd "s-l") 'flycheck-list-errors)
-(with-eval-after-load 'flycheck
-  (flycheck-pos-tip-mode))    ; show error/warning message at tooltip
+(use-package flycheck
+  :config
+  (global-flycheck-mode)
+  (global-set-key (kbd "s-n") 'flycheck-next-error)
+  (global-set-key (kbd "s-p") 'flycheck-previous-error)
+  (global-set-key (kbd "s-l") 'flycheck-list-errors))
+(use-package flycheck-pos-tip
+  :after (flycheck)
+  :config
+  (flycheck-pos-tip-mode))  ; show error/warning message at tooltip
